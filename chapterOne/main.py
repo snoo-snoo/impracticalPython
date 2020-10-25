a9ef878b886add37fc779adb4fec9ba3611a2725
Michael Plöckinger <mp@hejfish.com>
2020-10-25 09:56:54 +0100

    CH-01 add first- and surname tuple

---
+++ chapterOne/main.py
@@ -0,0 +1,21 @@
+""" CH-01 -Chapter One: Silly Name Generator """
+
+import sys, random
+
+print("Welcome to the Psych 'Sidekick Name Generator'.\n")
+print("A name just like Snoo would pick for Discostu.\n")
+
+first_names = (
+    'Janeth', 'Scarlet', 'Regan', 'Lyn', 'Carina', 'Jolanda', 'Sima', 'Preston', 'Flo', 'Kasey', 'Jamaal', 'Jeanine',
+    'Maura', 'Tyrell', 'Vickey', 'Elisha', 'Britta', 'Aimee', 'Ladawn', 'Adam', 'Octavia', 'Keena', 'Otto', 'Reinaldo',
+    'Gwenn', 'Sunny', 'Latrisha', 'Vanesa', 'Emerson', 'Tiara', 'Jamila', 'Refugio', 'Tami', 'Aida', 'Angelic', 'Angle',
+    'Pennie', 'Wilbur', 'Savannah', 'Phung', 'Rashad', 'Silvia', 'Lorene', 'Nelda', 'Paulene', 'Molly', 'Cherrie',
+    'Ivana',
+    'Jong', 'Reggie', 'Aero', 'Buddy', 'Bucky', 'Zorg', 'Dippy', 'Zee Zee', 'Kiddo', 'Reddy', 'Zip Zap', 'Zingo',
+    'Zero',
+    'Thunder', 'Beefy', 'Tramps', 'Waldo', 'Junior', 'Boop', 'Snips', 'Baby-Oil', 'Salte', 'Wheels', 'Pops')
+
+surnames = (
+    'Lightning-Leopard', 'Mega-Wraith', 'Blue Blockbuster', 'Golden Gastropod', 'Conquering Queen', 'Miltiversio',
+    'Night Champion', 'Mighy Dynamo', 'Super-ARcher', 'Space Mermaid', 'Dinosaur Damsel', 'Laser-Eyes'
+)
