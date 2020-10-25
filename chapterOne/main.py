d30c16de8b1ed395f3a022d491e5268cc5f115b8
Michael Plöckinger <mp@hejfish.com>
2020-10-25 10:13:04 +0100

    CH-01 install pylint and create an own convention file

--- chapterOne/main.py
+++ chapterOne/main.py
@@ -1,38 +1,48 @@
 """ CH-01 -Chapter One: Silly Name Generator """
 
-import sys, random
+import sys
+import random
 
 print('Welcome to the Psych \'Sidekick Name Generator\'.\n')
-print('A name just like Snoo would pick for Discostu.\n'')
+print('A name just like Snoo would pick for Discostu.\n')
 
-first_names = (
-    'Janeth', 'Scarlet', 'Regan', 'Lyn', 'Carina', 'Jolanda', 'Sima', 'Preston', 'Flo', 'Kasey', 'Jamaal', 'Jeanine',
-    'Maura', 'Tyrell', 'Vickey', 'Elisha', 'Britta', 'Aimee', 'Ladawn', 'Adam', 'Octavia', 'Keena', 'Otto', 'Reinaldo',
-    'Gwenn', 'Sunny', 'Latrisha', 'Vanesa', 'Emerson', 'Tiara', 'Jamila', 'Refugio', 'Tami', 'Aida', 'Angelic', 'Angle',
-    'Pennie', 'Wilbur', 'Savannah', 'Phung', 'Rashad', 'Silvia', 'Lorene', 'Nelda', 'Paulene', 'Molly', 'Cherrie',
-    'Ivana',
-    'Jong', 'Reggie', 'Aero', 'Buddy', 'Bucky', 'Zorg', 'Dippy', 'Zee Zee', 'Kiddo', 'Reddy', 'Zip Zap', 'Zingo',
-    'Zero',
-    'Thunder', 'Beefy', 'Tramps', 'Waldo', 'Junior', 'Boop', 'Snips', 'Baby-Oil', 'Salte', 'Wheels', 'Pops')
 
-surnames = (
-    'Lightning-Leopard', 'Mega-Wraith', 'Blue Blockbuster', 'Golden Gastropod', 'Conquering Queen', 'Miltiversio',
-    'Night Champion', 'Mighy Dynamo', 'Super-ARcher', 'Space Mermaid', 'Dinosaur Damsel', 'Laser-Eyes',
-    'Bigmeat', 'Bloominshine', 'Boogerbottom', 'Breedslovetrout', 'Buttergaugh', 'Clovenhoff', 'Clutterbuck',
-    'Cocktoasten', 'Endicott', 'Fewhairs', 'Hooperbag', 'Jenkins', 'Jefferson', 'Kingfish', 'Listenbee', 'McFadden',
-    'Moonshine', 'Oxhandler', 'Pealike', 'Pennywhistle', 'Winterkorn', 'Woolysocks'
-)
+def main():
+    first_names = (
+        'Janeth', 'Scarlet', 'Regan', 'Lyn', 'Carina', 'Jolanda', 'Sima', 'Preston', 'Flo', 'Kasey', 'Jamaal',
+        'Jeanine',
+        'Maura', 'Tyrell', 'Vickey', 'Elisha', 'Britta', 'Aimee', 'Ladawn', 'Adam', 'Octavia', 'Keena', 'Otto',
+        'Reinaldo',
+        'Gwenn', 'Sunny', 'Latrisha', 'Vanesa', 'Emerson', 'Tiara', 'Jamila', 'Refugio', 'Tami', 'Aida', 'Angelic',
+        'Angle',
+        'Pennie', 'Wilbur', 'Savannah', 'Phung', 'Rashad', 'Silvia', 'Lorene', 'Nelda', 'Paulene', 'Molly', 'Cherrie',
+        'Ivana',
+        'Jong', 'Reggie', 'Aero', 'Buddy', 'Bucky', 'Zorg', 'Dippy', 'Zee Zee', 'Kiddo', 'Reddy', 'Zip Zap', 'Zingo',
+        'Zero',
+        'Thunder', 'Beefy', 'Tramps', 'Waldo', 'Junior', 'Boop', 'Snips', 'Baby-Oil', 'Salte', 'Wheels', 'Pops')
 
-while True:
-    first_name = random.choice(first_names)
-    surname = random.choice(surnames)
+    surnames = (
+        'Lightning-Leopard', 'Mega-Wraith', 'Blue Blockbuster', 'Golden Gastropod', 'Conquering Queen', 'Miltiversio',
+        'Night Champion', 'Mighy Dynamo', 'Super-ARcher', 'Space Mermaid', 'Dinosaur Damsel', 'Laser-Eyes',
+        'Bigmeat', 'Bloominshine', 'Boogerbottom', 'Breedslovetrout', 'Buttergaugh', 'Clovenhoff', 'Clutterbuck',
+        'Cocktoasten', 'Endicott', 'Fewhairs', 'Hooperbag', 'Jenkins', 'Jefferson', 'Kingfish', 'Listenbee', 'McFadden',
+        'Moonshine', 'Oxhandler', 'Pealike', 'Pennywhistle', 'Winterkorn', 'Woolysocks'
+    )
 
-    print('\n\n')
-    print('{} {}'.format(first_name, surname), file=sys.stderr)
-    print('\n\n')
+    while True:
+        first_name = random.choice(first_names)
+        surname = random.choice(surnames)
 
-    try_again = input('\n\nTry again? (Press Enter else n to quit)\n ')
-    if try_again.lower() == 'n':
-        break
+        print('\n\n')
+        print('{} {}'.format(first_name, surname), file=sys.stderr)
+        print('\n\n')
 
-input('\nPress Enter to exit.')
+        try_again = input('\n\nTry again? (Press Enter else n to quit)\n ')
+        if try_again.lower() == 'n':
+            break
+
+    input('\nPress Enter to exit.')
+
+
+if __name__ == '__main__':
+    main()
