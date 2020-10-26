d1da3002e4e74f22bfa97c4017f99df998f2c537
Michael Plöckinger <mp@bissanzeiger.net>
2020-10-26 19:06:59 +0100

    CH-01-1 provide functionality to form a piglatin from a word, check for vowels or consonants first

---
+++ pigLatin/main.py
@@ -0,0 +1,26 @@
+""" Pig Latin Generator """
+
+VOWELS = ('a', 'e', 'i', 'o', 'u')
+
+
+def form_pig_latin():
+    """Take a user input and form a pig latin word."""
+    word = input("Please input a word: \n")
+
+    if isinstance(word, str):
+
+        if not word.startswith(VOWELS):
+            pig_latin = word[1:len(word)] + word[0] + "ay"
+            print(pig_latin)
+        else:
+            pig_latin = word + "way"
+
+        print(pig_latin)
+
+    else:
+        print("The given input looks like its not a word, please try again.")
+        form_pig_latin()
+
+
+if __name__ == '__main__':
+    form_pig_latin()
